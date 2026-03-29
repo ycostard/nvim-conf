5c25074b8dbaf613ce63e1c478fb315dd69fe381
Yohann <yohann@mac-2.home>
2026-03-29 03:38:46 +0200

    conf prête

--- lua/plugins/copilot-chat.lua
+++ lua/plugins/copilot-chat.lua
@@ -1,13 +1,15 @@
 return {
   {
     "CopilotC-Nvim/CopilotChat.nvim",
+    branch = "canary",
+    cmd = "CopilotChat",
     dependencies = {
       { "zbirenbaum/copilot.lua" },
       { "nvim-lua/plenary.nvim" },
     },
     opts = {},
     keys = {
-      { "<leader>cc", ":CopilotChat<CR>", desc = "Copilot Chat" },
+      { "<leader>cc", "<cmd>CopilotChat<CR>", desc = "Copilot Chat" },
     },
   },
 }
